1e58e5b0e8a008759a9fce7828dbce5f6c9068eb
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-08-21 12:21:41 +0900

    adding dependencies + change the endpoint to Groq so I don't have to pay

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,4 +2,10 @@ class Chat < ApplicationRecord
   acts_as_chat
   belongs_to :challenge
   has_many :messages, dependent: :destroy
+
+  after_initialize :set_chat
+
+  def set_chat
+    @chat = RubyLLM.chat(model: "openai/gpt-oss-20b", provider: :openai, assume_model_exists: true)
+  end
 end
